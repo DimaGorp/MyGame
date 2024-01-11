1841af3b1c57719d43128e034d551c8d57db644f
DimaGorp <72267870+DimaGorp@users.noreply.github.com>
2024-01-11 19:21:14 +0200

    added widgets

---
+++ Source/MyProject/PlayerWidget.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PlayerWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API UPlayerWidget : public UUserWidget
+{
+	GENERATED_BODY()
+protected:
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UProgressBar* HelthBar;
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	class UTextBlock* Helth_Text;
+public:
+	void SetHelth(float cur_helth, float max);
+	void SetText(float cur_helth);
+
+};
