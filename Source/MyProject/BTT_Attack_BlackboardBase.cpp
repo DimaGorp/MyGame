f2db3f67f1885d31f07e73df15ad9f56afe721ea
DimaGorp <72267870+DimaGorp@users.noreply.github.com>
2024-01-18 12:46:06 +0200

    Revert "Merge branch 'level-design'"
    This reverts commit b69c0ffc7d06ca611bf0d3d67237fa198bbc74f9, reversing
    changes made to a58b474fa5bd6fc9d43a07fc491cf3acebf927fc.

---
+++ Source/MyProject/BTT_Attack_BlackboardBase.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTT_Attack_BlackboardBase.h"
+#include "NavigationSystem.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "MyCharacter.h"
+#include "AI_01.h"
+#include "AI_Controller.h"
+UBTT_Attack_BlackboardBase::UBTT_Attack_BlackboardBase()
+{
+	NodeName = "AtackPlayer";
+}
+
+EBTNodeResult::Type UBTT_Attack_BlackboardBase::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AAI_Controller* owner = Cast<AAI_Controller>(OwnerComp.GetAIOwner());
+	if (owner == nullptr) {
+		return EBTNodeResult::Failed;
+	}
+	AAI_01* ai = Cast<AAI_01>(owner->GetPawn());
+	if (ai) {
+		ai->PlayAnim();
+	}
+	return EBTNodeResult::Succeeded;
+}
